9bba2b13a7991cb6277520109d8aed1598df3158
he <slut@vip.qq.com>
2022-11-21 15:29:31 +0800

    init

---
+++ src/test/java/com/example/javaeedemo/JavaEeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaeedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
